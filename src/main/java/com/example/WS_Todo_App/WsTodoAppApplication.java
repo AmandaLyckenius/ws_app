fe528fe9e6e65d78f0044e234e100dd9e2612aac
isabellaTroncoso <isabella.troncoso@stud.sti.se>
2025-09-05 09:15:00 +0200

    Initial commit

---
+++ src/main/java/com/example/WS_Todo_App/WsTodoAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.WS_Todo_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsTodoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsTodoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/WS_Todo_App/WsTodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WS_Todo_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsTodoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
