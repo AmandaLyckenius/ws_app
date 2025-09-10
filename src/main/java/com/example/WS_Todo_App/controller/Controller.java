b1493480eef67bff9615cfa6b32bf114afebf132
isabellaTroncoso <isabella.troncoso@stud.sti.se>
2025-09-10 15:28:39 +0200

    Add GET /all and GET /search by title endpoints with ResponseEntity

--- src/main/java/com/example/WS_Todo_App/controller/Controller.java
+++ src/main/java/com/example/WS_Todo_App/controller/Controller.java
@@ -5,6 +5,8 @@ import com.example.WS_Todo_App.repository.TaskRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/tasks")
 public class Controller {
@@ -18,4 +20,25 @@ public class Controller {
         Task save=repo.save(task);
         return ResponseEntity.status(201).body(save);
     }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Task>> getAllTasks() {
+        List<Task> tasks = repo.findAll();
+
+        if (tasks.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(tasks);
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<Task>> getTasksByTitle(@RequestParam String title) {
+        List<Task> tasks = repo.findByTitle(title);
+
+        if (tasks.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(tasks);
+    }
 }
+
