3c03d72f45d1d8ead488759482c7e52d6a27b8a2
amandalyckenius <alyckenius@gmail.com>
2025-09-16 14:05:10 +0200

    add: fintTopNTitles to repository (#11)
    Method to find top common titles with dynamic limit of titles

--- src/main/java/com/example/WS_Todo_App/repository/TaskRepository.java
+++ src/main/java/com/example/WS_Todo_App/repository/TaskRepository.java
@@ -1,6 +1,7 @@
 package com.example.WS_Todo_App.repository;
 
 import com.example.WS_Todo_App.model.Task;
+import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -13,4 +14,14 @@ public interface TaskRepository extends MongoRepository<Task, String> {
 
     @Query("{}")
     List<Task> findTopFive(Pageable pageable);
+
+    @Aggregation(pipeline = {
+            "{ $match: { title: { $ne: null, $ne: \"\" } } }",
+            "{ $project: { normalizedTitle: { $toLower: { $trim: { input: \"$title\" } } } } }",
+            "{ $group: { _id: \"$normalizedTitle\", count: { $sum: 1 } } }",
+            "{ $sort: { count: -1, _id: 1 } }",
+            "{ $limit: ?0 }",
+            "{ $project: { _id: 0, title: \"$_id\", count: 1 } }"
+    })
+    List<TitleCount> findTopNTitles(int n);
 }
