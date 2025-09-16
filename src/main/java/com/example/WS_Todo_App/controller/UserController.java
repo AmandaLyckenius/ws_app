32c48b6a36bd7aa16a3ccbec9f013294e8c4697d
isabellaTroncoso <isabella.troncoso@stud.sti.se>
2025-09-16 16:30:49 +0200

    Add: Usercontroller (refs #17)
    To create users and get them to connect user with tasks

---
+++ src/main/java/com/example/WS_Todo_App/controller/UserController.java
@@ -0,0 +1,41 @@
+package com.example.WS_Todo_App.controller;
+
+import com.example.WS_Todo_App.model.TaskUser;
+import com.example.WS_Todo_App.repository.TaskUserRepository;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final TaskUserRepository taskUserRepo;
+
+    public UserController(TaskUserRepository taskUserRepo) {
+        this.taskUserRepo = taskUserRepo;
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<TaskUser> createUser(@Valid @RequestBody TaskUser user) {
+        TaskUser saved = taskUserRepo.save(user);
+        return ResponseEntity.status(201).body(saved);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<TaskUser>> getAllUsers() {
+        List<TaskUser> users = taskUserRepo.findAll();
+        if (users.isEmpty())
+            return ResponseEntity.noContent().build();
+
+        return ResponseEntity.ok(users);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TaskUser> getUserById(@PathVariable String id) {
+        return taskUserRepo.findById(id).map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+}
